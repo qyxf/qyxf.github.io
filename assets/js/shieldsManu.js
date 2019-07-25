7d01bea5686dc331f0e8dcdea613878b2009949d
xjtu-blacksmith <youjiarui189@stu.xjtu.edu.cn>
2019-07-25 10:13:19 -0500

    JS: adding shields-processing codes

---
+++ assets/js/shieldsManu.js
@@ -0,0 +1,25 @@
+$(function() {
+    $("img[src='shield']").each(function()
+    {
+        $(this).attr("src", "https://img.shields.io/badge/" + $(this).attr("alt"));
+    });
+
+    $("img[src='person']").each(function()
+    {
+        var label = /\S*\d{2,3}/.exec($(this).attr("alt"));
+        var message = /\d{2,3}-?\S*/.exec($(this).attr("alt"));
+        message = message.toString().replace(/[-\d]/g,'');
+        var labelColor = "";
+        if(/钱/.test(label)){ labelColor = "yellow"; }
+        else if(/少/.test(label)){ labelColor = "red"; }
+        else if(/宗濂/.test(label)){ labelColor = "violet"; }
+        else if(/越杰/.test(label)){ labelColor = "brightgreen"; }
+        else if(/数\S*试/.test(label)){ labelColor = "lightblue"; }
+        else if(/物\S*试/.test(label)){ labelColor = "blue"; }
+        else if(/化\S*生/.test(label)){ labelColor = "blueviolet"; }
+        else if(/计\S*试/.test(label)){ labelColor = "orange"; }
+        else if(/人\S*试/.test(label)){ labelColor = "silver"; }
+        else { labelColor = "grey"; }
+        $(this).attr("src", "https://img.shields.io/badge/" + label + "-" + message + "-grey?labelColor=" + labelColor);
+    });
+});
