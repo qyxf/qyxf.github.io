7f1c16ef7bf0dfcd9c653c0289ebd6ea1099492e
xjtu-blacksmith <youjiarui189@stu.xjtu.edu.cn>
2019-08-07 13:53:04 -0500

    Modify target pos of nav menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,7 @@ $(function() {
   });
 
   $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 190;
+    var position = $($(this).attr("href")).offset().top;
     $("html, body").animate({scrollTop: position}, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
