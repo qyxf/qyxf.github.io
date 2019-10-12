0d4f9a1558eea84b3895d21f5f15d66de329d27b
xjtu-blacksmith <youjiarui189@stu.xjtu.edu.cn>
2019-10-12 12:16:09 +0800

    Update filter for renshi

--- assets/js/shieldsManu.js
+++ assets/js/shieldsManu.js
@@ -7,7 +7,7 @@ $(function() {
         var message = /\d{2,3}\S*/.exec(this.innerText);
         message = message.toString().replace(/[-\d]/g,'');
         var labelColor = "";
-        var classList = new Array(/钱/, /少/, /宗濂/, /越杰/, /数\S*试/, /物\S*试/, /化\S*生/, /计\S*试/, /人\S*试/);
+        var classList = new Array(/钱/, /少/, /宗濂/, /越杰/, /数\S*试/, /物\S*试/, /化\S*生/, /计\S*试/, /人\S*[试智]/);
         var classNameList = new Array("qian", "shao", "zonglian", "yuejie", "shushi", "wushi", "huasheng", "jishi", "renshi")
         var className = "other";
         for(var i = 0; i < classList.length; i++)
