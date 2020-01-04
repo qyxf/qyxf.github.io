8f9174cc422270a1df49a0c1207a735973dc5aef
xjtu-blacksmith <youjiarui189@stu.xjtu.edu.cn>
2020-01-04 12:33:31 +0800

    Finish theme setting

--- assets/js/set-theme.js
+++ assets/js/set-theme.js
@@ -5,14 +5,15 @@ function checkTheme() {
     $.cookie("theme", "white", { expires: 7, path: "/" });
   } else {
     if (theme == "black") {
-      $(".mdui-color-white").addClass("mdui-color-grey-800");
-      $(".mdui-color-grey-800").removeClass("mdui-color-white");
+      $(".mdui-color-white").addClass("mdui-theme-layout-dark");
+      $(".mdui-theme-layout-dark").removeClass("mdui-color-white");
       $(".mdui-theme-accent-indigo").addClass("mdui-theme-accent-cyan")
       $(".mdui-theme-accent-cyan").removeClass("mdui-theme-accent-indigo")
       $("#theme-btn").prop("checked", true);
+
     } else {
-      $(".mdui-color-grey-800").addClass("mdui-color-white");
-      $(".mdui-color-white").removeClass("mdui-color-grey-800");
+      $(".mdui-theme-layout-dark").addClass("mdui-color-white");
+      $(".mdui-color-white").removeClass("mdui-theme-layout-dark");
       $(".mdui-theme-accent-cyan").addClass("mdui-theme-accent-indigo")
       $(".mdui-theme-accent-indigo").removeClass("mdui-theme-accent-cyan")
       $("#theme-btn").prop("checked", false);
